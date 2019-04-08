bd2ffaee5997d4540ec68daeb1ce8507935f07c2
ionutnechita <nechitaionut97@live.com>
2019-04-08 16:32:49 +0300

    Initial commit

---
+++ src/main/java/SSVVL4MV/App.java
@@ -0,0 +1,13 @@
+package SSVVL4MV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
